d0ac08d953e49f45e87b3655985a49dbae78020e
mrcut <xxmrcutxx@gmail.com>
2022-11-24 10:34:58 +0100

    fix bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,39 +4,44 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () =>
+      import('./home/home.module').then((m) => m.HomePageModule),
   },
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
-  // {
-
-  //   path: 'bateaux',
-  //   loadChildren: () => import('./bateaux/bateaux.module').then( m => m.BateauxPageModule)
-  // },
+  {
+    path: 'bateaux',
+    loadChildren: () =>
+      import('./bateaux/bateaux.module').then((m) => m.BateauxPageModule),
+  },
   {
     path: 'single-bateau',
-    loadChildren: () => import('./single-bateau/single-bateau.module').then( m => m.SingleBateauPageModule)
+    loadChildren: () =>
+      import('./single-bateau/single-bateau.module').then(
+        (m) => m.SingleBateauPageModule
+      ),
   },
   {
     path: 'contact',
-    loadChildren: () => import('./contact/contact.module').then( m => m.ContactPageModule)
-
+    loadChildren: () =>
+      import('./contact/contact.module').then((m) => m.ContactPageModule),
   },
   {
     path: 'single-produit',
-    loadChildren: () => import('./single-produit/single-produit.module').then( m => m.SingleProduitPageModule)
+    loadChildren: () =>
+      import('./single-produit/single-produit.module').then(
+        (m) => m.SingleProduitPageModule
+      ),
   },
-
-
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
