26706cea30e064f3308880258fe7eb2492c85206
CestMoiChrist <svay.christloyd@gmail.com>
2022-11-25 11:26:56 +0100

    travail

--- src/app/single-produit/single-produit.page.ts
+++ src/app/single-produit/single-produit.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-single-produit',
@@ -8,27 +9,39 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class SingleProduitPage implements OnInit {
   produit!: {
-    id:            string;
-    name:          string;
-    category:      number;
-    price:         number;
-    unit:          number;
-    availability:  boolean;
-    sale:          boolean;
-    discount:      number;
-    comments:      string;
-    owner:         string;
-   }
+    id: string;
+    name: string;
+    category: number;
+    price: number;
+    unit: number;
+    availability: boolean;
+    sale: boolean;
+    discount: number;
+    comments: string;
+    owner: string;
+  }
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  constructor(private route: ActivatedRoute, private storage: Storage, private router: Router) { }
 
   ngOnInit() {
-      this.route.queryParams.subscribe(params => {
-        if(this.router.getCurrentNavigation()!.extras.state){
-          this.produit = this.router.getCurrentNavigation()!.extras.state!['produit'];
-          console.log(this.produit);
-        }
+    this.route.queryParams.subscribe(params => {
+      if (this.router.getCurrentNavigation()!.extras.state) {
+        this.produit = this.router.getCurrentNavigation()!.extras.state!['produit'];
+        console.log(this.produit);
+      }
     });
+  }
+
+  addToCart(): void {
+    this.storage.create();
+    this.storage.set("produit", "Test")
+      .then(value => alert("La valeur est " + value))
+      .catch(err => console.log(err));
+  }
+  getCartElement(): void {
+    this.storage.get("test")
+      .catch(err => console.log(err))
+      .then(value => ("La valeur est " + value))
   }
 
 }
