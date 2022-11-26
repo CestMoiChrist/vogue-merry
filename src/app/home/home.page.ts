2526b1d9d0a63c3e9ca7a3c80890dba8e09c7ac0
xxmrcutxx@gmail.com <xxmrcutxx@gmail.com>
2022-11-26 13:13:38 +0100

    fix homepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,74 +1,76 @@
 import { Component } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  urliconbato: string = "../assets/icon/bateau.png"
+  urliconbato: string = '../assets/icon/bateau.png';
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
-  onLoadProduit(produit: { name: string; description: string[], isOn: boolean }) {
+  onLoadProduit(produit: {
+    name: string;
+    description: string[];
+    isOn: boolean;
+  }) {
     let navigationExtras: NavigationExtras = {
       state: {
-        produit: produit
-      }
+        produit: produit,
+      },
     };
     this.router.navigate(['produit'], navigationExtras);
   }
 
-  onLoadRestaurant(restaurant: { name: string; description: string[], isOn: boolean }) {
+  onLoadRestaurant(restaurant: {
+    name: string;
+    description: string[];
+    isOn: boolean;
+  }) {
     let navigationExtras: NavigationExtras = {
       state: {
-        restaurant: restaurant
-      }
+        restaurant: restaurant,
+      },
     };
     this.router.navigate(['restaurant'], navigationExtras);
   }
 
-  onLoadBateau(bateau: { name: string; description: string[], isOn: boolean }) {
+  onLoadRecette(recette: {
+    name: string;
+    description: string[];
+    isOn: boolean;
+  }) {
     let navigationExtras: NavigationExtras = {
       state: {
-        bateau: bateau
-      }
-    };
-    this.router.navigate(['bateaux'], navigationExtras);
-  }
-
-  onLoadRecette(recette: { name: string; description: string[], isOn: boolean }) {
-    let navigationExtras: NavigationExtras = {
-      state: {
-        recette: recette
-      }
+        recette: recette,
+      },
     };
     this.router.navigate(['recette'], navigationExtras);
   }
 
   onGotoContact() {
-    this.router.navigate(['/contact'])
+    this.router.navigate(['/contact']);
   }
 
   onGotoListeProduits() {
-    this.router.navigate(['/liste-produits'])
+    this.router.navigate(['/liste-produits']);
   }
 
   onGotoCategorieProduits() {
-    this.router.navigate(['/categorie-produit'])
+    this.router.navigate(['/categorie-produit']);
   }
 
   onGoToBateaux() {
-    this.router.navigate(['/bateaux'])
-  }
-  
-  onGoToRestaurants() {
-    this.router.navigate(['/restaurants'])
+    this.router.navigate(['/bateaux']);
   }
 
-  onGotoRecette(){
-    this.router.navigate(['/list-recette'])
+  onGoToRestaurants() {
+    this.router.navigate(['/restaurants']);
+  }
+
+  onGotoRecette() {
+    this.router.navigate(['/list-recette']);
   }
 }
